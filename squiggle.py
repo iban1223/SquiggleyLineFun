3925bcbf5d6bb47ed0600d4e9be3d771d1c6d322
iban1223 <dmoses@oberlin.edu>
2022-03-14 16:21:04 -0400

    better random function implemented

--- squiggle.py
+++ squiggle.py
@@ -6,26 +6,26 @@ import random
 def squiggle(xS, yS, xE, yE, n, fileName):
 	file = open(fileName, "a")
 	#Setting up variables for making the line
-	rand = random.randrange(0,2) #Random number for making squigle
 	xIncrement = (xE-xS)/n #Number the x coord will be incremented by each iteration
 	yIncrement = (yE-yS)/n #Number the y coord will be incremented by each iteration
+	xRand = random.uniform(0,(2*xIncrement)) #X random number for making squigle
+	yRand = random.uniform(0,(2*yIncrement)) #Y random number for making squigle
 	xNew = xS #New x coord
 	yNew = yS #New y coord
-	file.write("\n")
 	#Starting the drawing of a newpath
-	file.write("newpath\n")
+	file.write("\nnewpath\n")
 	file.write(str(xNew) + " " + str(yNew) + " moveto\n") #Moving to start point
-	xNew = xS+xIncrement+rand
-	yNew = yS+yIncrement+rand
+	xNew = xS+xIncrement+xRand
+	yNew = yS+yIncrement+yRand
 	#Looping to create the line
 	for i in range(n):
 		#Writing to next point
-		file.write(str(xNew) + " " + str(yNew) + " lineto\n")
+		file.write(str(xNew+xRand) + " " + str(yNew+yRand) + " lineto\n")
 		#Generating coords for next point
-		rand = random.randrange(0,2)
-		xNew = xNew+xIncrement+rand
-		rand = random.randrange(0,2)
-		yNew = yNew+yIncrement+rand
+		xRand = random.uniform(-1,1)
+		xNew = xNew+xIncrement
+		yRand = random.uniform(-1,1)
+		yNew = yNew+yIncrement
 	#Closing the line and the file
 	file.write("stroke\n")
 	file.close()
@@ -39,8 +39,11 @@ class squiggle():
 	file.write("%!PS-Adobe-3.0 EPSF-3.0\n%%BoundingBox: 0 0 300 300\n\n1 setlinejoin\n1 setlinecap\n")
 	file.close()
 
+	y = 0
 
-	squiggle(0,0,150,150, 500, fileName)
+	for i in range(150):
+		squiggle(0,y,300,y, 300, fileName)
+		y += 4
 
 	file = open (fileName, "a")
 
